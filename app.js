c8b849ed9d48eec1d20d6b8ee7d0de5eda3d9c5d
Raymond Loman <raymond@carpago.nl>
2018-10-03 13:44:41 +0200

    Modify rest api to allow all in CORS

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var bodyParser = require('body-parser');
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/users', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
