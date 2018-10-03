b3960e264e49f71fdacb1616d1ed90562b72e270
Raymond Loman <raymond@carpago.nl>
2018-10-03 17:17:28 +0200

    All done until DELETE hence POST even rocks

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ var bodyParser = require('body-parser');
 
 var users = [];
 
+var lastId = -1;
+
 // parse application/json
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       this.users = JSON.parse(data);
-	console.log(this.users);
-	console.log(typeof(this.users));
-	next();
-   });
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+    this.users = JSON.parse(data);
+    console.log(this.users);
+    console.log(typeof(this.users));
+    this.lastId = this.users.length;
+    next();
+  });
 });
 
 app.use(function(req, res, next) {
@@ -24,46 +27,44 @@ app.use(function(req, res, next) {
 });
 
 
-app.get('/api/users', function (req, res) {
-         res.setHeader('Content-Type', 'application/json')
-         console.log(this.users);
-         res.end( JSON.stringify(this.users) );
+app.get('/api/users', function(req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  console.log(this.users);
+  res.end(JSON.stringify(this.users));
 });
 
-app.get('/api/users/:id', function (req, res) {
-   // First read existing users.
-      var user = this.users[+ req.params.id-1] 
-      res.setHeader('Content-Type', 'application/json')
-      res.end( JSON.stringify(user));
+app.get('/api/users/:id', function(req, res) {
+  // First read existing users.
+  var user = this.users[+req.params.id - 1]
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(user));
 });
 
 
-app.post('/api/users', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       data["user9"] = req.body;
-       console.log( data );
+app.post('/api/users', function(req, res) {
 
-       // res.end(JSON.stringify(data, null, 2));
-        res.setHeader('Content-Type', 'application/json')
-       res.end( JSON.stringify(data));
-   });
+  let user = req.body;
+  user.id = ++this.lastId
+  this.users.push(user);
+
+  // res.end(JSON.stringify(data, null, 2));
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(user));
 });
 
-app.delete('/api/users/:id', function (req, res) {
+app.delete('/api/users/:id', function(req, res) {
 
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       delete data["user" + req.params.id];
-       
-       console.log( data );
-       res.end( JSON.stringify(data));
-   });
+  // First read existing users.
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+    data = JSON.parse(data);
+    delete data["user" + req.params.id];
+
+    console.log(data);
+    res.end(JSON.stringify(data));
+  });
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, function() {
 
   var host = server.address().address
   var port = server.address().port
