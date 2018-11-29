03130e93776cfcc0881b05241db6764695be0b59
Raymond Loman <raymond@carpago.nl>
2018-11-29 10:38:16 +0100

    Set allow origin host to  localhost

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 // for now this is to say that everyone can reach this webserver
 // from everywhere
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "localhost");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
